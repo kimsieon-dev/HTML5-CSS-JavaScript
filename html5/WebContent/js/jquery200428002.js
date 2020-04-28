82776e7e7b0da23b820f1b77f75eef48cde20511
KimSiEon <kimsieon.dev@gmail.com>
2020-04-28 15:50:17 +0900

    jQuery - 배너 만들기

--- html5/WebContent/js/jquery200428002.js
+++ html5/WebContent/js/jquery200428002.js
@@ -1,4 +1,6 @@
 $(function(){
+	/* 
+	 * container의 위치를 이용하는 배너
 	let bannerAction = setInterval(move, 3000);
 	let position = 0;
 	$("section:first").hover(function(){
@@ -14,7 +16,8 @@ $(function(){
 	
 	$("button:first").click(function(){
 		position = --position % 3;
-		$(".container").stop(true).animate({"left" : 640 * position + "px"}, 1000);
+		if(position < 0) position = 2;
+		$(".container").stop(true).animate({"left" : -640 * position + "px"}, 1000);
 		clearInterval(bannerAction);
 		bannerAction = setInterval(move, 3000);
 	});
@@ -25,4 +28,43 @@ $(function(){
 		clearInterval(bannerAction);
 		bannerAction = setInterval(move, 3000);
 	});
+});
+*/
+	let bannerAction = setInterval(move, 3000);
+	
+	$("section:first").hover(function(){
+		clearInterval(bannerAction);
+	}, function(){
+		bannerAction = setInterval(move, 3000);
+	});
+	
+	function move() {
+		if($(".container").queue().length > 0) {
+			return false;
+		}
+		$(".container").animate({"left" : "-640px"}, 1000, function(){
+			$(".container > div:first").appendTo(".container");
+			$(".container").css("left","0");
+		});
+	}
+	
+	$("button:first").hover(function(){
+		if($(".container").queue().length > 0) {
+			return false;
+		}
+		$(".container > div:last").prependTo(".container");
+		$(".container").css("left", "-640px");
+		$(".container").animate({"left" : 0}, 1000);
+		console.log($(".container").queue().length);
+		clearInterval(bannerAction);
+		bannerAction = setInterval(move, 3000);
+	});
+	
+	
+	$("button:last").click(function(){
+		move();
+		clearInterval(bannerAction);
+		bannerAction = setInterval(move, 3000);
+	});
+	
 });
