d6302227cbcf19989362d88ff6906f9157ec8886
KimSiEon <kimsieon.dev@gmail.com>
2020-06-05 15:58:34 +0900

    imagesLoaded

---
+++ html5/WebContent/js/plugin001.js
@@ -0,0 +1,43 @@
+$(function() { 
+	images = [
+		"../images/pg.jpg",
+		"../images/pg2.jpg",
+		"../images/pg3.jpg",
+		"../images/pg4.jpg",
+		"../images/pg5.jpg"
+	];
+	
+	var clickCnt = 0;
+	
+	$("#imgDel").click(function() {
+		clickCnt = 0;
+		$(".picture").empty();
+	});
+	
+	$("#imgAdd").click(function() {
+		for(var i = images.length * clickCnt++; i < images.length * clickCnt; i++) {
+			$(".picture").append("<div><img src='" + images[i % images.length] + "'/></div>");
+		}
+		
+		var addImg = (clickCnt - 1) == 0 ? "" : (":gt(" + (images.length * (clickCnt - 1) -1)) + ")";
+		$(".picture > div" + addImg).each(function() {
+			var thisImg = $(this);
+			thisImg.imagesLoaded()
+				.always(function() {
+					console.log("always");
+				})
+				.done(function() {
+					console.log("done");
+//					thisImg.find(".loader").hide();
+				})
+				.fail(function() {
+					console.log("fail");
+					thisImg.hide();
+				})
+				.progress(function() {
+					console.log("progress");
+					thisImg.prepend("<div class='loader' > Loading...</div>");
+				});
+		});
+	});
+});
